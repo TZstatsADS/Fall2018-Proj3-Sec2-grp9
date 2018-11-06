d5c8267bcc940ab6971ec640c626da5307ba3c22
Levichasedream <42635712+Levichasedream@users.noreply.github.com>
2018-11-06 16:03:48 -0500

    Add files via upload

--- lib/superResolution.R
+++ lib/superResolution.R
@@ -5,7 +5,6 @@
 ### Author: Chengliang Tang
 ### Project 3
 library(dplyr)
-library(foreach)
 zero_pad <- function(array3)
 {
   dim1 <- dim(array3)[1]
@@ -30,7 +29,6 @@ findNeighb <- function(ind,mat)
   neigh7 <- mat[s+1, t] - mat[s , t]
   neigh8 <- mat[s+1, t+1] - mat[s , t]
   return(c(neigh1, neigh2, neigh3, neigh4, neigh5, neigh6, neigh7, neigh8))}
-
 reallocate_single <- function(index, row_rep, col_rep)
 {
   row1=row_rep[index[1]]
@@ -60,6 +58,7 @@ reallocate <- function(preArray, lh, lw){
 
 
 
+
 superResolution <- function(LR_dir, HR_dir, modelList){
   
   ### Construct high-resolution images from low-resolution images with trained predictor
@@ -78,17 +77,17 @@ superResolution <- function(LR_dir, HR_dir, modelList){
     featMat <- array(NA, c(dim(imgLR)[1] * dim(imgLR)[2], 8, 3))
     x <- dim(imgLR)[1]
     y <- dim(imgLR)[2]
-    imgLR <- zero_pad(imgLR)
-   
-
+    imgLR_zero <- zero_pad(imgLR)
     
-    inds <- expand.grid(2:(dim(imgLR)[2]-1), 2:(dim(imgLR)[1]-1))[,2:1] %>% 
-            as.matrix()
+    
+    
+    inds <- expand.grid(2:(dim(imgLR_zero)[2]-1), 2:(dim(imgLR_zero)[1]-1))[,2:1] %>% 
+      as.matrix()
     ### step 1. for each pixel and each channel in imgLR:
     ###           save (the neighbor 8 pixels - central pixel) in featMat
     ###           tips: padding zeros for boundary points
     for (k in 1:3) {
-      featMat[,,k] <- apply(inds, 1, findNeighb,imgLR[,,k] )
+      featMat[,,k] <- apply(inds, 1, findNeighb,imgLR_zero[,,k] )
       
     }
     
@@ -96,15 +95,17 @@ superResolution <- function(LR_dir, HR_dir, modelList){
     ### step 2. apply the modelList over featMat
     predMat <- test(modelList, featMat)
     ### step 3. recover high-resolution from predMat and save in HR_dir
-    prearray <- array(preMat, c(dim(imgLR)[1]*2,dim(imgLR)[2]*2,3))
-    recover <- reallocate(prearray, x,y )
+    imageArr <- array(NA, c(dim(predMat)[1],4,3))
+    imageArr_central  <- as.numeric(imgLR)
+    for (i in 1:3 ){
+      imageArr[,,i] <- predMat[,,i] +imageArr_central[((i-1)*dim(predMat)[1]+1):(dim(predMat)[1]*i)]
+    }
     
+    recover <-  Image(transpose(reallocate(imageArr, dim(imgLR)[2], dim(imgLR)[1])), colormode = Color)
     
-    
-    img <- Image(recover, colormode=Color)
-    writeImage(img,pathHR)
+    writeImage(recover,pathHR)
   }
+  
 }
-
 
 
