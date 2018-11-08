18cf2579af7aba8511bfc7eebd6f348ca8cbc5cc
Levichasedream <42635712+Levichasedream@users.noreply.github.com>
2018-11-08 10:56:03 -0500

    Add files via upload

---
+++ lib/depth/feature.R
@@ -0,0 +1,112 @@
+#############################################################
+### Construct features and responses for training images###
+#############################################################
+
+### Authors: Chengliang Tang/Tian Zheng
+### Project 3
+
+
+#############################################################
+### Construct features and responses for training images###
+#############################################################
+
+### Authors: Chengliang Tang/Tian Zheng
+### Project 3
+#############################################################
+### Construct features and responses for training images###
+#############################################################
+
+### Authors: Chengliang Tang/Tian Zheng
+### Project 3
+set.seed(357)
+zero_pad <- function(array3)
+{
+  dim1 <- dim(array3)[1]
+  dim2 <- dim(array3)[2]
+  new_array <- array(0,c(dim1+2, dim2+2, 3))
+  for (i in 1:3)
+  {
+    new_array[2:(dim1+1), 2:(dim2+1),i] <- array3[,,i]
+  }
+  return(new_array)
+}
+
+findNeighb <- function(ind,mat)
+{
+  s=ind[1];t=ind[2]
+  neigh1 <- mat[s-1, t-1] - mat[s, t]
+  neigh2 <- mat[s-1, t] - mat[s, t]
+  neigh3 <- mat[s-1, t+1] - mat[s, t]
+  neigh4 <- mat[s, t-1] - mat[s,t]
+  neigh5 <- mat[s, t+1] - mat[s , t]
+  neigh6 <- mat[s+1, t-1] - mat[s , t]
+  neigh7 <- mat[s+1, t] - mat[s , t]
+  neigh8 <- mat[s+1, t+1] - mat[s , t]
+  return(c(neigh1, neigh2, neigh3, neigh4, neigh5, neigh6, neigh7, neigh8))}
+
+
+label <- function(ind, matHR,matLR){
+  s=ind[1];t=ind[2]
+  lab=c(matHR[2*s-1, 2*t-1], matHR[2*s-1, 2*t], matHR[2*s, 2*t-1], matHR[2*s, 2*t])-matLR[s,t]
+  return(lab)
+}
+
+
+
+
+
+feature <- function(LR_dir, HR_dir, n_points=1000){
+  
+  ### Construct process features for training images (LR/HR pairs)
+  
+  ### Input: a path for low-resolution images + a path for high-resolution images 
+  ###        + number of points sampled from each LR image
+  ### Output: an .RData file contains processed features and responses for the images
+  
+  ### load libraries
+  library("EBImage")
+  set.seed(357)
+  library(dplyr)
+  n_files <- length(list.files(LR_dir))
+  #n_files=1
+  ### store feature and responses
+  featMat <- array(NA, c(n_files * n_points, 8, 3))
+  labMat <- array(NA, c(n_files * n_points, 4, 3))
+  ### read LR/HR image pairs
+  for(i in 1:n_files){
+    imgLR <- readImage(paste0(LR_dir,  "img_", sprintf("%04d", i), ".jpg"))
+    imgHR <- readImage(paste0(HR_dir,  "img_", sprintf("%04d", i), ".jpg"))
+    imgLR<-as.array(imgLR)
+    imgHR<-as.array(imgHR) 
+    ### step 1. sample n_points from imgLR
+    x <- sample(1:(dim(imgLR)[1]), n_points, replace = TRUE)
+    y <- sample(1:(dim(imgLR)[2]), n_points, replace = TRUE)
+    inds <- cbind(x, y)
+    
+    imgLR_zero <- zero_pad(imgLR)
+    ###
+    
+    
+    for(k in 1:3){
+      featMat[((i-1)*1000+1):(i*1000),,k] <-  apply(inds+1,1, findNeighb,imgLR_zero[,,k] )%>%t()
+      labMat[((i-1)*1000+1):(i*1000),,k] <- apply(inds, 1, label, imgHR[,,k],imgLR[,,k])%>%t()
+      
+    }
+    ### step 2. for each sampled point in imgLR,
+    
+    
+    
+    ### step 2.1. save (the neighbor 8 pixels - central pixel) in featMat
+    ###           tips: padding zeros for boundary points
+    
+    ### step 2.2. save the corresponding 4 sub-pixels of imgHR in labMat
+    
+    ### step 3. repeat above for three channels
+    
+  }
+  return(list(feature = featMat, label = labMat))
+}
+
+
+
+
---
+++ lib/depth/superResolution.R
@@ -0,0 +1,111 @@
+########################
+### Super-resolution ###
+########################
+
+### Author: Chengliang Tang
+### Project 3
+library(dplyr)
+zero_pad <- function(array3)
+{
+  dim1 <- dim(array3)[1]
+  dim2 <- dim(array3)[2]
+  new_array <- array(0,c(dim1+2, dim2+2, 3))
+  for (i in 1:3)
+  {
+    new_array[2:(dim1+1), 2:(dim2+1),i] <- array3[,,i]
+  }
+  return(new_array)
+}
+
+findNeighb <- function(ind,mat)
+{
+  s=ind[1];t=ind[2]
+  neigh1 <- mat[s-1, t-1] - mat[s, t]
+  neigh2 <- mat[s-1, t] - mat[s, t]
+  neigh3 <- mat[s-1, t+1] - mat[s, t]
+  neigh4 <- mat[s, t-1] - mat[s,t]
+  neigh5 <- mat[s, t+1] - mat[s , t]
+  neigh6 <- mat[s+1, t-1] - mat[s , t]
+  neigh7 <- mat[s+1, t] - mat[s , t]
+  neigh8 <- mat[s+1, t+1] - mat[s , t]
+  return(c(neigh1, neigh2, neigh3, neigh4, neigh5, neigh6, neigh7, neigh8))}
+reallocate_single <- function(index, row_rep, col_rep)
+{
+  row1=row_rep[index[1]]
+  row=2*(row1-1) + rep(1:2, each=2)[index[2]]
+  col1=col_rep[index[1]]
+  col=2*(col1-1)+rep(1:2,2)[index[2]]
+  return(c(row, col))
+}
+
+reallocate <- function(preArray, lh, lw){
+  row_rep <- rep(1:lh, each=lw)
+  col_rep <- rep(1:lw, lh)
+  inds = expand.grid(1:4, 1:(lh*lw))[,2:1]%>% as.matrix()
+  
+  imgArray <- array(NA, c(2*lh,2*lw, 3))
+  for (i in 1:(lh*lw*4)){
+    a <- inds[i,1]
+    b <- inds[i,2]
+    index1 <- reallocate_single(inds[i,], row_rep , col_rep)
+    a1 <- index1[1]
+    b1 <- index1[2]
+    imgArray[a1,b1,] <- preArray[a,b,]
+  }
+  return(imgArray)
+}
+
+
+
+
+
+superResolution <- function(LR_dir, HR_dir, modelList){
+  
+  ### Construct high-resolution images from low-resolution images with trained predictor
+  
+  ### Input: a path for low-resolution images + a path for high-resolution images 
+  ###        + a list for predictors
+  
+  ### load libraries
+  library("EBImage")
+  n_files <- length(list.files(LR_dir))
+  
+  ### read LR/HR image pairs
+  for(i in 1:n_files){
+    imgLR <- readImage(paste0(LR_dir,  "img", "_", sprintf("%04d", i), ".jpg"))
+    pathHR <- paste0(HR_dir,  "img", "_", sprintf("%04d", i), ".jpg")
+    featMat <- array(NA, c(dim(imgLR)[1] * dim(imgLR)[2], 8, 3))
+    x <- dim(imgLR)[1]
+    y <- dim(imgLR)[2]
+    imgLR_zero <- zero_pad(imgLR)
+    
+    
+    
+    inds <- expand.grid(2:(dim(imgLR_zero)[1]-1), 2:(dim(imgLR_zero)[2]-1))%>% 
+      as.matrix()
+    ### step 1. for each pixel and each channel in imgLR:
+    ###           save (the neighbor 8 pixels - central pixel) in featMat
+    ###           tips: padding zeros for boundary points
+    for (k in 1:3) {
+      featMat[,,k] <- apply(inds, 1, findNeighb,imgLR_zero[,,k] )
+      
+    }
+    
+    
+    ### step 2. apply the modelList over featMat
+    predMat <- test(modelList, featMat)
+    ### step 3. recover high-resolution from predMat and save in HR_dir
+    imageArr <- array(NA, c(dim(predMat)[1],4,3))
+    imageArr_central  <- as.numeric(imgLR)
+    for (i in 1:3 ){
+      imageArr[,,i] <- predMat[,,i] +imageArr_central[((i-1)*dim(predMat)[1]+1):(dim(predMat)[1]*i)]
+    }
+    
+    recover <-  Image(transpose(reallocate(imageArr, dim(imgLR)[2], dim(imgLR)[1])), colormode = Color)
+    
+    writeImage(recover,pathHR)
+  }
+  
+}
+
+
