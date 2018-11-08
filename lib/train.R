21551cc9bb0f6c5933392df386c0b10c69f530e6
Levichasedream <42635712+Levichasedream@users.noreply.github.com>
2018-11-08 13:53:56 -0500

    Add files via upload

---
+++ lib/train.R
@@ -0,0 +1,36 @@
+##########################################################
+### Train a classification model with training features ###
+#########################################################
+
+### Author: Chengliang Tang
+
+
+train<- function(dat_train, label_train, par=NULL){
+  ### creat model list
+  modelList <- list()
+  
+  ### Train with gradient boosting model
+  if(is.null(par)){
+    depth <- 1
+  } else {
+    depth <- par$depth
+  }
+  
+  
+  k1=rep(1:4,3)
+  k2=rep(1:3,each=4)
+  modelList<-foreach(i=1:12,.packages="gbm") %dopar%{ 
+    
+    featMat <- dat_train[, , k2[i]]
+    labMat <- label_train[, k1[i], k2[i]] 
+    fit_gbm=gbm.fit(x=featMat, y=labMat,
+                    n.trees=200,
+                    distribution="gaussian",
+                    interaction.depth=depth, 
+                    bag.fraction = 0.5,
+                    shrinkage = 0.1,
+                    verbose=FALSE)
+    list(fit=fit_gbm,iter=gbm.perf(fit_gbm, method="OOB", plot.it = FALSE))
+  }
+  return(modelList)
+}
