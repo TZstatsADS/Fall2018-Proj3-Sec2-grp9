f3a294e3a122726fbc74653a024102253d27597b
yw3085 <43450287+yw3085@users.noreply.github.com>
2018-11-08 13:14:22 -0500

    Add files via upload

--- lib/cross_validation_xgb.R
+++ lib/cross_validation_xgb.R
@@ -5,7 +5,7 @@
 ### Author: Chengliang Tang
 ### Project 3
 
-XGBcv.function <- function(X.train, y.train, d, K){
+XGBcv.function <- function(X.train, y.train, par, K){ 
   
   n <- dim(y.train)[1]
   n.fold <- floor(n/K)
@@ -18,11 +18,29 @@ XGBcv.function <- function(X.train, y.train, d, K){
     test.data <- X.train[s == i, ,]
     test.label <- y.train[s == i, ,]
     
-    par <- list(depth=d)
-    fit <- XGBTr(train.data, train.label, par)
-    pred <- XGBtest(fit, test.data)  
+    fit <- XGBTr(train.data, train.label, par=NULL)
+    pred <- XGBtest0(fit, test.data)
     cv.error[i] <- mean((pred - test.label)^2)  
     
   }			
   return(c(mean(cv.error),sd(cv.error)))
 }
+
+
+############run test##############
+# 
+# model_values <- seq(3, 11, 2)
+# model_labels = paste("XGB with depth =", model_values)
+# 
+# feat_train <- features$feature
+# label_train <- features$label
+# 
+# x.train = feat_train
+# y.train = label_train
+# 
+# err_cv <- array(dim=c(length(model_values), 2))
+# 
+# for(k in 1:length(model_values)){
+#   cat("k=", k, "\n")
+#   err_cv[k,] <- XGBcv.function(feat_train, label_train, model_values[k], K = 3)
+# }
--- lib/test_xgboost.R
+++ lib/test_xgboost.R
@@ -25,36 +25,39 @@ XGBtest <- function(modelList, dat_test){
     c1 <- (i-1) %% 4 + 1
     c2 <- (i-c1) %/% 4 + 1
     featMat <- dat_test[, , c2]
+    
+    #featMat <- dat_test[,2:25, c2]
+    #center<-dat_test[,1,c2]
     ### make predictions
-    predArr[, c1, c2] <- predict(fit_train[[1]], newdata=featMat)
-  }
-  return(as.numeric(predArr))
-}
-
-
-
-XGBtestTry <- function(modelList, dat_test){
-  
-  ### Fit the classfication model with testing data
-  
-  ### Input: 
-  ###  - the fitted classification model list using training data
-  ###  - processed features from testing images 
-  ### Output: training model specification
-  
-  ### load libraries
-  library("xgboost")
-  
-  predArr <- array(NA, c(dim(dat_test)[1], 4, 3))
-  
-  for (i in 1:12){
-    fit_train <- modelList[[i]]
-    ### calculate column and channel
-    c1 <- (i-1) %% 4 + 1
-    c2 <- (i-c1) %/% 4 + 1
-    featMat <- dat_test[, , c2]
-    ### make predictions
-    predArr[, c1, c2] <- predict(fit_train[[1]], newdata=featMat)
+    predArr[, c1, c2] <- predict(fit_train$fit, newdata=featMat)#+center
   }
   return(predArr)
 }
+
+
+
+# XGBtestTry <- function(modelList, dat_test){
+#   
+#   ### Fit the classfication model with testing data
+#   
+#   ### Input: 
+#   ###  - the fitted classification model list using training data
+#   ###  - processed features from testing images 
+#   ### Output: training model specification
+#   
+#   ### load libraries
+#   library("xgboost")
+#   
+#   predArr <- array(NA, c(dim(dat_test)[1], 4, 3))
+#   
+#   for (i in 1:12){
+#     fit_train <- modelList[[i]]
+#     ### calculate column and channel
+#     c1 <- (i-1) %% 4 + 1
+#     c2 <- (i-c1) %/% 4 + 1
+#     featMat <- dat_test[, , c2]
+#     ### make predictions
+#     predArr[, c1, c2] <- predict(fit_train[[1]], newdata=featMat)
+#   }
+#   return(predArr)
+# }
